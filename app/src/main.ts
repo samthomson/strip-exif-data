f3aa28a2d187d902202c47c7452c0b0a542e7c14
Sam <samt@samt.st>
2022-02-23 17:03:06 +0000

    check for env vars and rename command

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,3 +1,16 @@
 import * as Module from './module'
 
-console.log('got a result from the module method:', Module.method())
+const checkForEnvVarsOrExit = (): void => {
+	const { DIR_IN, DIR_OUT } = process.env
+	if (!DIR_IN || !DIR_OUT) {
+		console.log('env vars must be set')
+		process.exit()
+	}
+}
+
+const main = () => {
+	checkForEnvVarsOrExit()
+	console.log('made it past initial var check')
+}
+
+main()
